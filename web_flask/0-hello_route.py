6b94e381dce4e2976d94cf6ba52c9bbb1b6e19ed
sam-fsd <kabirusam037@gmail.com>
2023-10-20 10:08:14 -0700

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """ Runs when the route is accessd
@@ -14,6 +15,7 @@ def hello():
         String. A text to display
     """
     return f'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
